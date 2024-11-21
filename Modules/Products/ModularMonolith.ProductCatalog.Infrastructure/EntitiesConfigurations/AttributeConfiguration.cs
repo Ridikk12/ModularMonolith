dc90764ea247ade98b3fa5bb8112a30153488fa1
Norbert Debosz <nordeb@auditdata.com>
2024-11-21 16:34:25 +0000

    feat: UI

---
+++ Modules/Products/ModularMonolith.ProductCatalog.Infrastructure/EntitiesConfigurations/AttributeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModularMonolith.Products.Domain.Entities.Attributes;
+
+namespace ModularMonolith.ProductCatalog.Infrastructure.EntitiesConfigurations;
+
+public class AttributeConfiguration : IEntityTypeConfiguration<Attribute>
+{
+    public void Configure(EntityTypeBuilder<Attribute> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.OwnsMany(x=> x.Values, navigationBuilder =>
+        {
+            navigationBuilder.ToJson();
+        });
+
+    }
+}
---
+++ Modules/Products/ModularMonolith.ProductCatalog.Infrastructure/Startup/ProductsModuleStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ModularMonolith.Products.Application.Abstraction;
+using ModularMonolith.Products.Application.EventBus;
+using ModularMonolith.Products.Application.Features.Products.Create;
+
+namespace ModularMonolith.ProductCatalog.Infrastructure.Startup
+{
+    public static class ProductsModuleStartup
+    {
+        public static IServiceCollection AddProductModule(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(typeof(CreateProductCommand));
+
+            services.AddDbContext<ProductsModuleDbContext>(x =>
+            {
+                var connectionString = configuration["Modules:ProductsModule:DbConnectionString"];
+                x.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<IAttributeValidator, AttributeValidator>();
+            services.AddScoped<IProductModuleDbContext, ProductsModuleDbContext>();
+
+            services.AddScoped<IProductEventBus, ProductsModuleEventBus>();
+
+            return services;
+        }
+    }
+}
